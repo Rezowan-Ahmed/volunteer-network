866a1632ee0dc4d0677ed1f06022ed16a45aa3f9
Rezowan-Ahmed <ahmedrezowan07@gmail.com>
2020-10-03 09:13:19 +0800

    create fake data

---
+++ src/FakeData/volunteerCategories.js
@@ -0,0 +1,104 @@
+const volunteerCategories = [
+    {
+        id: '1',
+        name: "Child Support",
+        image: "https://i.ibb.co/4wqhr5z/child-Support.png"
+    },
+    {
+        id: '2',
+        name: "Refuge Shelter",
+        image: "https://i.ibb.co/1fHJ9gp/refuse-Shelter.png"
+    },
+    {
+        id: '3',
+        name: "Food Charity",
+        image: "https://i.ibb.co/XVxZCH8/food-Charity.png"
+    },
+    {
+        id: '4',
+        name: "Host a clothing swap",
+        image: "https://i.ibb.co/XS66Kzx/cloth-Swap.png"
+    },
+    {
+        id: '5',
+        name: "Host a river clean-up",
+        image: "https://i.ibb.co/4fLDg2x/river-Clean.png"
+    },
+    {
+        id: '6',
+        name: "Clean water for children",
+        image: "https://i.ibb.co/PgHQt2m/clean-Water.png"
+    },
+    {
+        id: '7',
+        name: "Good education",
+        image: "https://i.ibb.co/MncxbXk/good-Education.png"
+    },
+    {
+        id: 8,
+        name: "New books for children",
+        image: "https://i.ibb.co/DfVmfMS/newBooks.png"
+    },
+    {
+        id: '9',
+        name: "Host a study group",
+        image: "https://i.ibb.co/KLZ3DVb/study-Group.png"
+    },
+    {
+        id: '10',
+        name: "Build birdhouses for your neighbors",
+        image: "https://i.ibb.co/j59ZK4t/bird-House.png"
+    },
+    {
+        id: '11',
+        name: "Organize books at the library",
+        image: "https://i.ibb.co/RBRDLxL/library-Books.png"
+    },
+    {
+        id: '12',
+        name: "Give a seminar on driving safety",
+        image: "https://i.ibb.co/Rg1H14H/drive-Safety.png"
+    },
+    {
+        id: '13',
+        name: "Give free music lessons",
+        image: "https://i.ibb.co/w47NHPL/music-Lessons.png"
+    },
+    {
+        id: '14',
+        name: "Teach people how to register to vote",
+        image: "https://i.ibb.co/f1ZZtcY/vote-Register.png"
+    },
+    {
+        id: '15',
+        name: "Clean up your local park",
+        image: "https://i.ibb.co/7yLwfFr/clearn-Park.png"
+    },
+    {
+        id: '16',
+        name: "Give IT help to local adults",
+        image: "https://i.ibb.co/RNPCWCH/ITHelp.png"
+    },
+    {
+        id: '17',
+        name: "Foster a shelter animal",
+        image: "https://i.ibb.co/0jLJ9vv/animal-Shelter.png"
+    },
+    {
+        id: '18',
+        name: "Babysit during PTA meetings",
+        image: "https://i.ibb.co/fCYKDML/babySit.png"
+    },
+    {
+        id: '19',
+        name: "Collect stuffed animal",
+        image: "https://i.ibb.co/yyNZffL/stuffed-Animals.png"
+    },
+    {
+        id: '20',
+        name: "Collect school supplies",
+        image: "https://i.ibb.co/6PTtSQZ/school-Suffiles.png"
+    }
+];
+
+export default volunteerCategories;
